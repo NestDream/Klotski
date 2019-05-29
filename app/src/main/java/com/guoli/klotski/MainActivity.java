140f3fb96e0777953b647e711fa93240b91b2274
Li Guo <gl_1997@outlook.com>
2019-05-29 11:05:01 +0800

    加入首页的设置

--- app/src/main/java/com/guoli/klotski/MainActivity.java
+++ app/src/main/java/com/guoli/klotski/MainActivity.java
@@ -2,6 +2,9 @@ package com.guoli.klotski;
 
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,324 +12,122 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.guoli.klotski.R;
 
 public class MainActivity extends AppCompatActivity {
-    Button Qz[] = new Button[10];
-    int BG[][] = new int[5][4];
-    TextView txt1;
-    float SW;
-    float x1, x2, y1, y2;
-    int Step = 0;
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Qz[0] = (Button) findViewById(R.id.Qz1);
-        Qz[1] = (Button) findViewById(R.id.Qz2);
-        Qz[2] = (Button) findViewById(R.id.Qz3);
-        Qz[3] = (Button) findViewById(R.id.Qz4);
-        Qz[4] = (Button) findViewById(R.id.Qz5);
-        Qz[5] = (Button) findViewById(R.id.Qz6);
-        Qz[6] = (Button) findViewById(R.id.Qz7);
-        Qz[7] = (Button) findViewById(R.id.Qz8);
-        Qz[8] = (Button) findViewById(R.id.Qz9);
-        Qz[9] = (Button) findViewById(R.id.Qz10);
+    }
 
-        txt1 = (TextView) findViewById(R.id.Text1);
-        //注册监听器
-        for (int i = 0; i < 10; i++)
-            Qz[i].setOnTouchListener(new mTouch());
-        //背景数组对应填充
-        for (int i = 0; i < 5; i++)
-            for (int j = 0; j < 4; j++)
-                BG[i][j] = 1;
-        BG[4][1] = 0;
-        BG[4][2] = 0;
-        //输出屏幕宽度和
-        txt1.post(new Runnable() {
+    public void gaming_dialog(View view) {
+        final String[] items = {"游戏模式1", "游戏模式2", "游戏模式3", "游戏模式4"};
+        AlertDialog.Builder listDialog =
+                new AlertDialog.Builder(MainActivity.this);
+        listDialog.setTitle("选择游戏模式");
+        listDialog.setItems(items, new DialogInterface.OnClickListener() {
             @Override
-            public void run() {
-                txt1.setText("Screen Width:" + txt1.getWidth() + "; Qz Width" + Qz[1].getWidth());
-                SW = txt1.getWidth();
-                init();
+            public void onClick(DialogInterface dialog, int which) {
+//                // which 下标从0开始
+                switch (which) {
+                    case 0:
+                        Intent intent_0 = new Intent(MainActivity.this, GameActivity.class);
+                        startActivity(intent_0);
+                        MainActivity.this.finish();
+                        break;
+                    case 1:
+                        Intent intent_1 = new Intent(MainActivity.this, GameActivity.class);
+                        startActivity(intent_1);
+                        MainActivity.this.finish();
+                        break;
+                    case 2:
+                        Intent intent_2 = new Intent(MainActivity.this, GameActivity.class);
+                        startActivity(intent_2);
+                        MainActivity.this.finish();
+                        break;
+                    case 3:
+                        Intent intent_3 = new Intent(MainActivity.this, GameActivity.class);
+                        startActivity(intent_3);
+                        MainActivity.this.finish();
+                        break;
+                }
+//                // ...To-do
+//                Toast.makeText(MainActivity.this,
+//                        "你点击了" + items[which],
+//                        Toast.LENGTH_SHORT).show();
             }
         });
+        listDialog.show();
+    }
+
+    public void gameing_dialog2(View view) {
+        //定义一个新的对话框对象
+        AlertDialog.Builder alertdialogbuilder = new AlertDialog.Builder(this);
+        //设置对话框提示内容
+        alertdialogbuilder.setMessage("选择哪张地图？");
+        //定义对话框2个按钮标题及接受事件的函数
+        alertdialogbuilder.setNeutralButton("游戏地图1", game_1);
+        alertdialogbuilder.setNeutralButton("游戏地图2", game_2);
+        //创建并显示对话框
+        AlertDialog alertdialog1 = alertdialogbuilder.create();
+        alertdialog1.show();
 
     }
 
-    //监听实现
-    public class mTouch implements View.OnTouchListener {
-
+    private DialogInterface.OnClickListener game_1 = new DialogInterface.OnClickListener() {
+        //使用该标记是为了增强程序在编译时候的检查，如果该方法并不是一个覆盖父类的方法，在编译时编译器就会报告错误。
         @Override
-        public boolean onTouch(View v, MotionEvent event) {
-            int type; // 1 兵    2  张飞  3 关羽 4 曹操
-            int r, c;
-            if (v.getWidth() == v.getHeight()) {
-                if (v.getHeight() > 300)
-                    type = 4;//大的是曹操
-                else
-                    type = 1;//小的是小兵
 
-            } else {
-                if (v.getHeight() > v.getWidth())
-                    type = 2;//高大于宽是竖的
-                else
-                    type = 3;//宽大于高是横的
-            }
-
-            r = (int) (v.getY() / 270f);
-            c = (int) (v.getX() / 270f);
-            Log.d("mTouch", "User is touching at(" + r + "," + c + ")");//touch的时候的行列，row column
-
-            //继承了Activity的onTouchEvent方法，直接监听点击事件
-            if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                //当手指按下的时候
-                x1 = event.getX();
-                y1 = event.getY();
-            }
-
-            if (event.getAction() == MotionEvent.ACTION_UP) {
-                //当手指离开的时候
-                x2 = event.getX();
-                y2 = event.getY();
-                if (y1 - y2 > 50) //"向上滑:"
-                {
-                    switch (type) {
-                        case 1:
-                            if (r > 0 && BG[r - 1][c] == 0) {
-                                SetPois(v, r - 1, c);
-                                BG[r - 1][c] = 1;
-                                BG[r][c] = 0;
-                                Step++;
-                                txt1.setText("你已经走了：" + Step + "步！");
-                            }
-                            break;
-                        case 2:
-                            if (r > 0 && BG[r - 1][c] == 0) {
-                                SetPois(v, r - 1, c);
-                                BG[r - 1][c] = 1;
-                                BG[r + 1][c] = 0;
-                                Step++;
-                                txt1.setText("你已经走了：" + Step + "步！");
-                            }
-                            break;
-                        case 3:
-                            if (r > 0 && BG[r - 1][c] == 0 && BG[r - 1][c + 1] == 0) {
-                                SetPois(v, r - 1, c);
-                                BG[r - 1][c] = BG[r - 1][c + 1] = 1;
-                                BG[r][c] = BG[r][c + 1] = 0;
-                                Step++;
-                                txt1.setText("你已经走了：" + Step + "步！");
-                            }
-                            break;
-                        case 4:
-                            if (r > 0 && BG[r - 1][c] == 0 && BG[r - 1][c + 1] == 0) {
-                                SetPois(v, r - 1, c);
-                                BG[r - 1][c] = BG[r - 1][c + 1] = 1;
-                                BG[r + 1][c] = BG[r + 1][c + 1] = 0;
-                                Step++;
-                                txt1.setText("你已经走了：" + Step + "步！");
-                            }
-                            break;
-
-                    }
-
-                } else if (y2 - y1 > 50) { //向下滑
-                    switch (type) {
-                        case 1:
-                            if (r < 4 && BG[r + 1][c] == 0) {
-                                SetPois(v, r + 1, c);
-                                BG[r + 1][c] = 1;
-                                BG[r][c] = 0;
-                                Step++;
-                                txt1.setText("你已经走了：" + Step + "步！");
-                            }
-                            break;
-                        case 2:
-                            if (r < 3 && BG[r + 2][c] == 0) {
-                                SetPois(v, r + 1, c);
-                                BG[r + 2][c] = 1;
-                                BG[r][c] = 0;
-                                Step++;
-                                txt1.setText("你已经走了：" + Step + "步！");
-                            }
-
-                            break;
-                        case 3:
-                            if (r < 4 && BG[r + 1][c] == 0 && BG[r + 1][c + 1] == 0) {
-                                SetPois(v, r + 1, c);
-                                BG[r + 1][c] = BG[r + 1][c + 1] = 1;
-                                BG[r][c] = BG[r][c + 1] = 0;
-                                Step++;
-                                txt1.setText("你已经走了：" + Step + "步！");
-                            }
-                            break;
-                        case 4:
-                            if (r < 3 && BG[r + 2][c] == 0 && BG[r + 2][c + 1] == 0) {
-                                SetPois(v, r + 1, c);
-                                BG[r + 2][c] = BG[r + 2][c + 1] = 1;
-                                BG[r][c] = BG[r][c + 1] = 0;
-                                Step++;
-                                txt1.setText("你已经走了：" + Step + "步！");
-                                if (r + 1 == 3 && c == 1) {
-                                    Log.d("onWinning", "From up to down");
-                                    txt1.setText("你赢了！共用" + Step + "步！");
-                                    //此处应该转到另一个activity，胜利用新的activity或者一个toast体现
-                                    //记录用户时间和步数，如果top5，加入排行榜
-                                    //注意析构
-                                }
-                            }
-                            break;
-                    }
-                } else if (x1 - x2 > 50) //向左滑
-                {
-                    switch (type) {
-                        case 1:
-                            if (c > 0 && BG[r][c - 1] == 0) {
-                                SetPois(v, r, c - 1);
-                                BG[r][c - 1] = 1;
-                                BG[r][c] = 0;
-                                Step++;
-                                txt1.setText("你已经走了：" + Step + "步！");
-                            }
-                            break;
-                        case 2:
-                            if (c > 0 && BG[r][c - 1] == 0 && BG[r + 1][c - 1] == 0) {
-                                SetPois(v, r, c - 1);
-                                BG[r][c - 1] = 1;
-                                BG[r + 1][c - 1] = 1;
-                                BG[r][c] = 0;
-                                BG[r + 1][c] = 0;
-                                Step++;
-                                txt1.setText("你已经走了：" + Step + "步！");
-                            }
-                            break;
-                        case 3:
-                            if (c > 0 && BG[r][c - 1] == 0) {
-                                SetPois(v, r, c - 1);
-                                BG[r][c - 1] = 1;
-                                BG[r][c + 1] = 0;
-                                Step++;
-                                txt1.setText("你已经走了：" + Step + "步！");
-                            }
-                            break;
-                        case 4:
-                            if (c > 0 && BG[r][c - 1] == 0 && BG[r + 1][c - 1] == 0) {
-                                SetPois(v, r, c - 1);
-                                BG[r][c - 1] = BG[r + 1][c - 1] = 1;
-                                BG[r][c + 1] = BG[r + 1][c + 1] = 0;
-                                Step++;
-                                txt1.setText("你已经走了：" + Step + "步！");
-//                                if (r + 1 == 3 && c == 1) {
-                                if (r == 3 && c == 2) {
-                                    Log.d("onWinning", "From right to left");
-                                    txt1.setText("你赢了，共用" + Step + "步！");
-                                }
-                            }
-                            break;
-                    }
-                } else if (x2 - x1 > 50) //向右滑
-                {
-                    switch (type) {
-                        case 1:
-                            if (c < 3 && BG[r][c + 1] == 0) {
-                                SetPois(v, r, c + 1);
-                                BG[r][c + 1] = 1;
-                                BG[r][c] = 0;
-                                Step++;
-                                txt1.setText("你已经走了：" + Step + "步！");
-                            }
-                            break;
-                        case 2:
-                            if (c < 3 & BG[r][c + 1] == 0 && BG[r + 1][c + 1] == 0) {
-                                SetPois(v, r, c + 1);
-                                BG[r][c + 1] = 1;
-                                BG[r + 1][c + 1] = 1;
-                                BG[r][c] = 0;
-                                BG[r + 1][c] = 0;
-                                Step++;
-                                txt1.setText("你已经走了：" + Step + "步！");
-                            }
-                            break;
-                        case 3:
-                            if (c < 2 && BG[r][c + 2] == 0) {
-                                SetPois(v, r, c + 1);
-                                BG[r][c + 2] = 1;
-                                BG[r][c] = 0;
-                                Step++;
-                                txt1.setText("你已经走了：" + Step + "步！");
-                            }
-                            break;
-                        case 4:
-                            if (c < 2 && BG[r][c + 2] == 0 && BG[r + 1][c + 2] == 0) {
-                                SetPois(v, r, c + 1);
-                                BG[r][c + 2] = BG[r + 1][c + 2] = 1;
-                                BG[r][c] = BG[r + 1][c] = 0;
-                                Step++;
-                                txt1.setText("你已经走了：" + Step + "步！");
-//                                if (r + 1 == 3 && c == 1) {
-                                if (r == 3 && c == 0) {
-                                    Log.d("onWinning", "From left to right");
-                                    txt1.setText("你赢了！共用" + Step + "步！");
-                                }
-                            }
-                            break;
-                    }
-                }
-            }
-            return true;
+        public void onClick(DialogInterface arg0, int arg1) {
+            //当按钮click1被按下时执行结束进程
+            Intent intent = new Intent(MainActivity.this, GameActivity.class);
+            startActivity(intent);
+            MainActivity.this.finish();
         }
+    };
+    private DialogInterface.OnClickListener game_2 = new DialogInterface.OnClickListener() {
+        //使用该标记是为了增强程序在编译时候的检查，如果该方法并不是一个覆盖父类的方法，在编译时编译器就会报告错误。
+        @Override
+
+        public void onClick(DialogInterface arg0, int arg1) {
+            //当按钮click1被按下时执行结束进程
+            Intent intent = new Intent(MainActivity.this, GameActivity.class);
+            startActivity(intent);
+            MainActivity.this.finish();
+        }
+    };
+
+    public void exit_dialog(View view) {
+        //定义一个新的对话框对象
+        AlertDialog.Builder alertdialogbuilder = new AlertDialog.Builder(this);
+        //设置对话框提示内容
+        alertdialogbuilder.setMessage("确定要退出程序吗？");
+        //定义对话框2个按钮标题及接受事件的函数
+        alertdialogbuilder.setPositiveButton("确定", click_confirm);
+        alertdialogbuilder.setNegativeButton("取消", click_cancel);
+        //创建并显示对话框
+        AlertDialog alertdialog1 = alertdialogbuilder.create();
+        alertdialog1.show();
+
     }
 
-    /**
-     * 根据手机分辨率从DP转成PX
-     *
-     * @param context
-     * @param dpValue
-     * @return
-     */
-    public static int dip2px(Context context, float dpValue) {
-        float scale = context.getResources().getDisplayMetrics().density;
-        return (int) (dpValue * scale + 0.5f);
-    }
+    private DialogInterface.OnClickListener click_confirm = new DialogInterface.OnClickListener() {
+        //使用该标记是为了增强程序在编译时候的检查，如果该方法并不是一个覆盖父类的方法，在编译时编译器就会报告错误。
+        @Override
 
-    void SetSize(Button v, int w, int h, String txt) {
-        v.setWidth(240);//(w * dip2px(getApplicationContext(),SW/4));
-        v.setHeight(h * dip2px(getApplicationContext(), SW / 4));
-        v.setText(txt);
-    }
+        public void onClick(DialogInterface arg0, int arg1) {
+            //当按钮click1被按下时执行结束进程
+            android.os.Process.killProcess(android.os.Process.myPid());
+        }
+    };
 
-    void SetPois(View v, int r, int c) {
-        v.setX(c * SW / 4f);
-        v.setY(r * SW / 4f);
-    }
-
-    void init() {
-        SetSize(Qz[0], 1, 1, "兵");
-        SetPois(Qz[0], 4, 0);
-        SetSize(Qz[1], 1, 1, "兵");
-        SetPois(Qz[1], 3, 1);
-        SetSize(Qz[2], 1, 1, "兵");
-        SetPois(Qz[2], 3, 2);
-        SetSize(Qz[3], 1, 1, "兵");
-        SetPois(Qz[3], 4, 3);
-
-        SetSize(Qz[4], 1, 2, "张飞");
-        SetPois(Qz[4], 0, 0);
-        SetSize(Qz[5], 1, 2, "赵云");
-        SetPois(Qz[5], 0, 3);
-        SetSize(Qz[6], 1, 2, "马超");
-        SetPois(Qz[6], 2, 0);
-        SetSize(Qz[7], 1, 2, "黄忠");
-        SetPois(Qz[7], 2, 3);
-
-        SetSize(Qz[8], 2, 1, "关羽");
-        SetPois(Qz[8], 2, 1);
-        SetSize(Qz[9], 2, 2, "曹操");
-        SetPois(Qz[9], 0, 1);
-        //txt1.setText("SW：" +dip2px(getApplicationContext(), SW)+","+getApplicationContext().getResources().getDisplayMetrics().density);
-    }
+    private DialogInterface.OnClickListener click_cancel = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface arg0, int arg1) {
+            //当按钮click2被按下时则取消操作
+            arg0.cancel();
+        }
+    };
 }
