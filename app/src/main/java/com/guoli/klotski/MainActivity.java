3b578ca9f59d949ea8e29f2e22bcd0808e56a695
Li Guo <gl_1997@outlook.com>
2019-05-29 15:18:35 +0800

    update UI

--- app/src/main/java/com/guoli/klotski/MainActivity.java
+++ app/src/main/java/com/guoli/klotski/MainActivity.java
@@ -1,20 +1,13 @@
 package com.guoli.klotski;
 
 
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.guoli.klotski.R;
 
 public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,6 +53,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         listDialog.show();
+    }
+
+    public void about(View view) {
+        //定义一个新的对话框对象
+        AlertDialog.Builder alertdialogbuilder = new AlertDialog.Builder(this);
+        //设置对话框提示内容
+
+        alertdialogbuilder.setTitle("关于");
+        alertdialogbuilder.setMessage("制作者：郭立\n学号：1613545\n支持：南开大学软件学院\n开源素材来源：baidu.com");
+        //定义对话框2个按钮标题及接受事件的函数
+        alertdialogbuilder.setPositiveButton("确定", null);
+        //创建并显示对话框
+        AlertDialog alertdialog1 = alertdialogbuilder.create();
+        alertdialog1.show();
     }
 
     public void gameing_dialog2(View view) {
@@ -130,4 +137,6 @@ public class MainActivity extends AppCompatActivity {
             arg0.cancel();
         }
     };
+
+
 }
